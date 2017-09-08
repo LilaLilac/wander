5d439f1f5d418b4658580f80a9d29dcb83b354e1
LilaLilac <junehydrangea626@gmail.com>
2017-09-08 20:37:47 +0900

    ジャンプと横移動

---
+++ wander action/Assets/Scripts/playercontroller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour {
+
+    Rigidbody2D rigid2D;
+    float jumpForce = 300f;
+    float walkForce = 30f;
+    float maxWalkSpeed = 2f;
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+        //左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        if (speedx < this.maxWalkSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+        if(key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        this.animator.speed = speedx / 2f;
+	}
+}
